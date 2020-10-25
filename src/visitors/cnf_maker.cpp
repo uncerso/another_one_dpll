72a7e91f3a09f4ee162647965d09e3b820a82947
uncerso <yayayayaya38@gmail.com>
2020-10-25 20:14:36 +0300

    Improved cnf maker, added more tests

--- src/visitors/cnf_maker.cpp
+++ src/visitors/cnf_maker.cpp
@@ -1,6 +1,22 @@
 #include "cnf_maker.hpp"
 using namespace std;
 
+void add_disj(CNF & cnf, pair<size_t, bool> && a, pair<size_t, bool> && b) {
+    if (a.first == b.first && a.second != b.second)
+        return;
+    cnf.push_back({move(a), move(b)});
+}
+
+void add_disj(CNF & cnf, pair<size_t, bool> && a, pair<size_t, bool> && b, pair<size_t, bool> && c) {
+    bool p1 = a.first == b.first && a.second != b.second;
+    bool p2 = a.first == c.first && a.second != c.second;
+    bool p3 = c.first == b.first && c.second != b.second;
+    if (p1 || p2 || p3)
+        return;
+    cnf.push_back({move(a), move(b), move(c)});
+}
+
+
 CNF CNFMaker::make_CNF(CommonAST const & ast) {
     cnf = CNF();
     next_free_id = ast.var_to_id.size();
@@ -28,9 +44,9 @@ void CNFMaker::visit(Or & v)  {
     v.rhs->accept(*this);
     auto l2 = ret_literal;
     ret_literal = {p, false};
-    cnf.push_back({ret_literal.to_neg_pair(), l1.to_pair(), l2.to_pair()});
-    cnf.push_back({ret_literal.to_pair(), l1.to_neg_pair()});
-    cnf.push_back({ret_literal.to_pair(), l2.to_neg_pair()});
+    add_disj(cnf, ret_literal.to_neg_pair(), l1.to_pair(), l2.to_pair());
+    add_disj(cnf, ret_literal.to_pair(), l1.to_neg_pair());
+    add_disj(cnf, ret_literal.to_pair(), l2.to_neg_pair());
 }
 
 void CNFMaker::visit(And & v) {
@@ -40,7 +56,7 @@ void CNFMaker::visit(And & v) {
     v.rhs->accept(*this);
     auto l2 = ret_literal;
     ret_literal = {p, false};
-    cnf.push_back({ret_literal.to_pair(), l1.to_neg_pair(), l2.to_neg_pair()});
-    cnf.push_back({ret_literal.to_neg_pair(), l1.to_pair()});
-    cnf.push_back({ret_literal.to_neg_pair(), l2.to_pair()});
+    add_disj(cnf, ret_literal.to_pair(), l1.to_neg_pair(), l2.to_neg_pair());
+    add_disj(cnf, ret_literal.to_neg_pair(), l1.to_pair());
+    add_disj(cnf, ret_literal.to_neg_pair(), l2.to_pair());
 }
